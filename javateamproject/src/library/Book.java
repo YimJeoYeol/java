1637f7540cf3c3584e628a75b02688a72caa8801
임재열 <86841539+yimje@users.noreply.github.com>
2023-03-05 19:49:57 +0900

    Add files via upload

--- javateamproject/src/library/Book.java
+++ javateamproject/src/library/Book.java
@@ -18,8 +18,9 @@ public class Book {
                System.out.println("2. 도서정보수정");
                System.out.println("3. 도서검색");
                System.out.println("4. 도서전체조회");
-               System.out.println("5. 도서삭제");
-               System.out.println("6. 이전메뉴");
+               System.out.println("5. 연체 정보");
+               System.out.println("6. 도서삭제");
+               System.out.println("7. 이전메뉴");
 
                switch (sc.nextInt()) {
                        case 1:
@@ -46,14 +47,21 @@ public class Book {
                                        System.out.println("등록된 도서 정보가 없습니다.");
                                }
                                break;
-                       case 5:
+                       case 5: 
+                    	   if (bookSign.getBookNumList().size() > 0) {
+                    		   new SearchBook(bookSign.getBookNumList(), bookSign.getBookNameList(), bookSign.getRantList()).bookSearch();;
+                    	   }else {
+                    		   System.out.println("도서 연제정보가 없습니다.");
+                    	   }
+                    	   break;
+                       case 6:
                                if (bookSign.getBookNumList().size() > 0) {
                                        new BookWithdrawal(sc, bookSign.getBookNumList(), bookSign.getBookNameList(), null).BookWithdrawal();
                                } else {
                                        System.out.println("등록된 도서 정보가 없습니다.");
                                }
                                break;
-                       case 6:
+                       case 7:
                                Library.returnToMain();
                                break;
                        default:
